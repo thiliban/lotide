7316b22c7296330989e33486d266c9dd778ab48a
Thiliban Baskaran <thiliban.b@gmail.com>
2023-06-18 03:08:15 +0000

    Implement middle

--- middle.js
+++ middle.js
@@ -1,22 +1,8 @@
-const eqArrays = function(x, y) {
-    for (let i = 0; i < x.length; i++) {
-        if (x[i] === y[i]) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-}
-const assertArrayEqual = function(x,y) {
-    if (eqArrays(x,y) === true){
-        console.log(`✅✅✅ Assertion passed: ${x} === ${y}.`);
-    }
-    else {
-        console.log(`🛑🛑🛑 Assertion Failed: ${x} !== ${y}.`);
-    }
-}
-
+/*
+This function accepts an array, and returns the middle item/items of the array.
+If the number of items in the array are even, then it will return the 2 middle items,
+whereas if the number of items in the array are odd, it will return a single item.
+*/
 const middle = function (lst) {
     emptLst = [];
     if(lst.length < 3) {
@@ -29,9 +15,6 @@ const middle = function (lst) {
         return Math.round(lst.length/2);
     }
     
-}
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5]))
-console.log(middle([1])) 
-console.log(middle([1, 2]))
-console.log(middle([1,2,3,4]));
+};
+
+module.exports = middle;
