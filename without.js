9d47dcfe0b158c8210b3044d5db839ba39f28198
Thiliban Baskaran <thiliban.b@gmail.com>
2023-06-18 03:15:52 +0000

    Implement without

--- without.js
+++ without.js
@@ -1,22 +1,7 @@
-const eqArrays = function(x, y) {
-    for (let i = 0; i < x.length; i++) {
-        if (x[i] === y[i]) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-}
-const aAE = function(x,y) {
-    if (eqArrays(x,y) === true){
-        console.log(`✅✅✅ Assertion passed: ${x} === ${y}.`);
-    }
-    else {
-        console.log(`🛑🛑🛑 Assertion Failed: ${x} !== ${y}.`);
-    }
-}
-
+/*
+This function returns a new array with desired items removed from the original array
+with the items implemented in the parameter 'y'
+*/
 const without = function(x,y) {
     const nA = []
     for (let i = 0; i < x.length; i++) {
@@ -27,9 +12,5 @@ const without = function(x,y) {
     }
     return nA;
 }
-const words = ['hello', 'world', 'lighthouse'];
-console.log(aAE(words, ['hello', 'world', 'lighthouse']));
 
-console.log(without([1, 2, 3], [1])) // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
-console.log(without(["1", "2", "3"], ["1", "2", 3])) // => false
+module.exports = without;
