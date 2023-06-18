7d8d88e84b18f93865ea9e1a0db8910c4916e6cc
Thiliban Baskaran <thiliban.b@gmail.com>
2023-06-18 03:14:53 +0000

    Implement tail

--- tail.js
+++ tail.js
@@ -1,18 +1,11 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}.`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}.`);
-    }
-  };
-
+/*
+This function is used to return every item except for the first item in the 
+inputted array
+*/
 const tail = function(lst) {
     return lst.slice(1);
-}
+};
 
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
 
 
