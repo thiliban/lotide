cf4404f7356c8582751f1507075d2d65e3cd7857
Thiliban Baskaran <thiliban.b@gmail.com>
2023-06-18 03:05:07 +0000

    Implement findKeyByValue

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,25 +1,24 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}.`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}.`);
+/*
+This function is used to match the user input in 'y' to the original
+array in 'x', and will return the matched item
+*/
+const findKeyByValue = function(x, y) {
+  let nArr = Object.keys(x);
+  for (let item of nArr) {
+    if(x[item] === y) {
+      return item
     }
-  };
-
-  const findKeyByValue = function(x, y) {
-    let nArr = Object.keys(x);
-    for (let item of nArr) {
-            if(x[item] === y) {
-                return item
-            }
-    }
+  }
 };
+/*
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+*/
   
-  const bestTVShowsByGenre = { 
-    sci_fi: "The Expanse",
-    comedy: "Brooklyn Nine-Nine",
-    drama:  "The Wire"
-  };
-  
-  assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
-  assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.exports = findKeyByValue;
