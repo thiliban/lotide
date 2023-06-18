0ac944e1f8aad1ad3948185148ac61b04c1d1145
Thiliban Baskaran <thiliban.b@gmail.com>
2023-06-18 03:01:39 +0000

    Implement eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,27 +1,19 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}.`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}.`);
-    }
-  };
-    
+/*
+This function is used to determine whether two arrays are equal, and will return
+true if the arrays are equal, and false if the arrays are not equal
+*/
 const eqArrays = function(x, y) {
-    if(x === y) {
-        return true;
+  if(x === y) {
+    return true;
+  }
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] === y[i]) {
+      return true;
     }
-    for (let i = 0; i < x.length; i++) {
-        if (x[i] === y[i]) {
-            return true;
-        }
-        else {
-            return false;
-        }
+    else {
+      return false;
     }
-}
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+  }
+};
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(assertEqual(eqArrays([1,2,3],[1,2,3]), true));
-console.log(assertEqual(eqArrays([1,2,3],[3,2,1]), true));
+module.exports = eqArrays;
