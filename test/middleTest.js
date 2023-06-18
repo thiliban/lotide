268faa9f1b379e5b77387fd0c4a3a46b92b42f9d
Thiliban Baskaran <thiliban.b@gmail.com>
2023-06-18 03:16:55 +0000

    Implement test

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,1 +1,14 @@
-const middle = require('../middle.js')
+const assert = require('chai').assert;
+const middle = require('../middle.js');
+
+describe("#middle", () => {
+
+    it("returns [2] for [1, 2, 3]", () => {
+      assert.deepEqual(middle([1, 2, 3]), [2]);
+    });
+  
+    it("returns [2, 3] for [1, 2, 3, 4]", () => {
+      assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]); 
+    });
+  
+  });
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,1 +1,14 @@
+const assert = require('chai').assert;
 const tail = require('../tail.js');
+
+describe("#tail", () => {
+
+    it("returns [2, 3] for [1, 2, 3]", () => {
+      assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+    });
+  
+    it("returns '5' for ['5']", () => {
+      assert.deepEqual(tail([2, 5]), [5]); 
+    });
+  
+  });
