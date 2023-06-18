206fcb6344897367d1018bedccf1f209534da11c
Thiliban Baskaran <thiliban.b@gmail.com>
2023-06-18 03:04:43 +0000

    Implement findKey

--- findKey.js
+++ findKey.js
@@ -1,3 +1,7 @@
+/*
+This function is used to accept an 'object', and return the key of the position
+that has been entered by the user in 'callback'.
+*/
 const findKey = function(object, callback) {
     let nObj = Object.keys(object);
     for (let item of nObj) {
@@ -6,12 +10,15 @@ const findKey = function(object, callback) {
         }
     }
 };
+/*
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+*/
 
-console.log(findKey({
-    "Blue Hill": { stars: 1 },
-    "Akaleri":   { stars: 3 },
-    "noma":      { stars: 2 },
-    "elBulli":   { stars: 3 },
-    "Ora":       { stars: 2 },
-    "Akelarre":  { stars: 3 }
-  }, x => x.stars === 2) )
+module.exports = findKey;
